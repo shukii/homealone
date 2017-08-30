100d5b149499be7d09f7fac2c485d23f7a59c421
Gordon Brady <gordon.brady@gmail.com>
2017-08-30 13:41:06 +0000

    done

--- homealone.py
+++ homealone.py
@@ -25,7 +25,7 @@ count = 0
 print("System armed!")
 
 def captureAndTweet():
-    message = "Intruder alert!! Do you recognise this mug?"
+    message = "Intruder alert!! Do you recognise this mug @shukii @gordonbrady @katd24?"
     camera.capture("/home/pi/button.jpg")
     with open("/home/pi/button.jpg", 'rb') as photo:
         twitter.update_status_with_media(status=message, media=photo)
