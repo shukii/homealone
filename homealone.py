8c032642f3e65df266f8d39791b4fc11a9db61e1
Gordon Brady <gordon.brady@gmail.com>
2017-08-30 11:08:41 +0000

    works

--- homealone.py
+++ homealone.py
@@ -1,6 +1,6 @@
 from picamera import PiCamera
 from gpiozero import Button
-
+from gpiozero import MotionSensor 
 from twython import Twython
 
 from auth import (
@@ -19,10 +19,20 @@ twitter = Twython(
 
 camera = PiCamera()
 button = Button(27)
+sense = MotionSensor(21)
+count = 0
 
-button.wait_for_press()
-message = "Intruder alert!! Do you recognise this mug?"
-camera.capture("/home/pi/button.jpg")
-with open("/home/pi/button.jpg", 'rb') as photo:
-    twitter.update_status_with_media(status=message, media=photo)
-print("Tweeted: %s" % message)
+print("System armed!")
+
+def captureAndTweet():
+    message = "Intruder alert!! Do you recognise this mug?"
+    camera.capture("/home/pi/button.jpg")
+    with open("/home/pi/button.jpg", 'rb') as photo:
+        twitter.update_status_with_media(status=message, media=photo)
+    print("Tweeted: %s" % message)
+    global count 
+    count += 1
+
+while True and count < 3:
+    button.when_pressed = captureAndTweet
+    sense.when_motion = captureAndTweet
