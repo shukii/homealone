fdb1dcba0fed4eaafe0fc0ff55c6fdf8024ce5b2
katd1988 <k.davey21@gmail.com>
2017-08-30 10:18:47 +0000

    added auth

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'N3BpL42vtUd1kHh6yX9Z77uye'
+consumer_secret     = 'SvPsnekaN02SwlBb7hwdcbVP3IIyKXxdH4V9rin37mYqTmWJL1'
+access_token        = '902832524631658498-Ms7cb6K3vkRwJv15nDK0lgfDQMzj6IB'
+access_token_secret = 'QdM6BAHUY9CDJrCyT5AntWkJfgdVy8dNFrxWk0JWpSRun'
